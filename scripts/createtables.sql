fa0fcfad458f4988543474f41fd61bed42fa9e08
Siann <Siann@MacBook-Pro-de-Siann.local>
2023-12-31 12:27:47 +0200

    added 3 other tables for coherence with data in /data

--- scripts/createtables.sql
+++ scripts/createtables.sql
@@ -20,3 +20,29 @@ CREATE TABLE Utilisateurs (
     FOREIGN KEY (ID_Type) REFERENCES TypeUtilisateur(ID_Type)
 );
 
+CREATE TABLE Lieu (
+    ID_Lieu integer PRIMARY KEY,
+    Nom varchar(30) NOT NULL,
+    Adresse varchar(100),
+    Ville varchar(30) NOT NULL,
+    Pays varchar(30) NOT NULL,
+);
+
+CREATE TABLE Groupe(
+    ID_Groupe integer PRIMARY KEY,
+    Nom varchar(30) NOT NULL,
+    Descript varchar(200),
+);
+
+CREATE TABLE Concert(
+    ID_Concert integer PRIMARY KEY,
+    Nom varchar(30) NOT NULL,
+    Dates DATE NOT NULL,
+    Prix integer,
+    Nbr_places integer NOT NULL,
+    Cause varchar(30),
+    BesoinVolontaires integer,
+    EspacesExterieurs BOOLEAN,
+    Enfants BOOLEAN,
+    ID_Lieu FOREIGN KEY REFERENCES Lieu(ID_Lieu)
+);
