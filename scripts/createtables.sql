f0c64756dc1c6d2361ec4f868f9e111c89ddd775
Siann <Siann@MacBook-Pro-de-Siann.local>
2023-12-31 12:13:22 +0200

    added second table to test foreign keys

--- scripts/createtables.sql
+++ scripts/createtables.sql
@@ -1,14 +1,22 @@
+DROP TABLE TypeUtilisateur;
 DROP TABLE Utilisateurs;
 
 
-CREATE TABLE Utilisateurs (
-ID_Utilisateur integer primary key,
-Prenom varchar(30) NOT NULL,
-Nom_famille varchar(30) NOT NULL,
-Pseudo varchar(30) NOT NULL,
-Date_naissance DATE NOT NULL,
-Email varchar(30) NOT NULL,
-Mot_de_passe varchar(30) NOT NULL,
-Photo_profil bytea,
-ID_Type int NOT NULL
+CREATE TABLE TypeUtilisateur(
+    ID_Type integer PRIMARY KEY,
+    Libelle varchar(30)
 );
+
+CREATE TABLE Utilisateurs (
+    ID_Utilisateur integer PRIMARY KEY,
+    Prenom varchar(30) NOT NULL,
+    Nom_famille varchar(30) NOT NULL,
+    Pseudo varchar(30) NOT NULL,
+    Date_naissance DATE NOT NULL,
+    Email varchar(30) NOT NULL,
+    Mot_de_passe varchar(30) NOT NULL,
+    Photo_profil bytea,
+    ID_Type int NOT NULL,
+    FOREIGN KEY (ID_Type) REFERENCES TypeUtilisateur(ID_Type)
+);
+
