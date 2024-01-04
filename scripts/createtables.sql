53f2c9a36e75225189c87b938da232a4b85c12a8
Siann <Siann@MBP-de-Siann.lan>
2024-01-04 23:35:51 +0200

    needs debugging

--- scripts/createtables.sql
+++ scripts/createtables.sql
@@ -1,9 +1,5 @@
-DROP TABLE Concert CASCADE;
-DROP TABLE Lieu CASCADE;
-DROP TABLE Groupe CASCADE;
-DROP TABLE TypeUtilisateur CASCADE;
-DROP TABLE Utilisateurs CASCADE;
-
+DROP TABLE TypeUtilisateur, Utilisateurs, Lieu, Genre, Groupe, Album, Morceau, Playlist, Concert, LineUp, 
+Tag, Avis, Amis, Follows CASCADE;
 
 CREATE TABLE TypeUtilisateur(
     ID_Type integer PRIMARY KEY,
@@ -18,7 +14,7 @@ CREATE TABLE Utilisateurs(
     Date_naissance DATE NOT NULL,
     Email varchar(30) NOT NULL,
     Mot_de_passe varchar(30) NOT NULL,
-    Photo_profil bytea,
+    --Photo_profil bytea,
     ID_Type int NOT NULL,
     FOREIGN KEY (ID_Type) REFERENCES TypeUtilisateur(ID_Type)
 );
@@ -31,10 +27,35 @@ CREATE TABLE Lieu (
     Pays varchar(30) NOT NULL
 );
 
+CREATE TABLE Genre(
+    ID_Genre integer PRIMARY KEY,
+    Genre varchar(30) NOT NULL
+);
+
 CREATE TABLE Groupe(
     ID_Groupe integer PRIMARY KEY,
     Nom varchar(30) NOT NULL,
     Descript varchar(200)
+);
+
+CREATE TABLE Album(
+    ID_Album integer PRIMARY KEY,
+    Titre varchar(30) NOT NULL,
+    DateSortie date NOT NULL,
+    Duree time
+);
+
+CREATE TABLE Morceau(
+    ID_Album integer PRIMARY KEY,
+    Titre varchar(30) NOT NULL,
+    Duree time
+);
+
+CREATE TABLE Playlist(
+    ID_Playlist integer PRIMARY KEY,
+    Nom varchar(30) NOT NULL,
+    ID_Utilisateur integer,
+    FOREIGN KEY (ID_Utilisateur) REFERENCES Utilisateurs(ID_Utilisateur)
 );
 
 CREATE TABLE Concert(
@@ -49,4 +70,60 @@ CREATE TABLE Concert(
     Enfants BOOLEAN,
     ID_Lieu integer, 
     FOREIGN KEY (ID_Lieu) REFERENCES Lieu(ID_Lieu)
+);
+
+CREATE TABLE LineUp(
+    ID_LineUp integer PRIMARY KEY,
+    ID_Concert integer,
+    ID_Groupe integer,
+    Position integer,
+    UNIQUE(ID_Concert, ID_Groupe, Position),
+    FOREIGN KEY ID_Concert REFERENCES Concert(ID_Concert),  
+    FOREIGN KEY ID_Groupe REFERENCES Groupe(ID_Groupe)
+);
+
+CREATE TABLE Tag(
+    ID_Tag integer PRIMARY KEY,
+    Libelle varchar(30) NOT NULL
+);
+
+CREATE TABLE Avis(
+    ID_Avis integer PRIMARY KEY,
+    Note integer,
+    Commentaire varchar(1200),
+    DatePost Date NOT NULL,
+    ID_Utilisateur integer NOT NULL,
+    ID_Concert integer,
+    ID_Groupe integer,
+    ID_LineUp integer
+    ID_Playlist integer,
+    ID_Lieu integer
+
+    FOREIGN KEY ID_Utilisateur REFERENCES Utilisateur(ID_Utilisateur) 
+    FOREIGN KEY ID_Concert REFERENCES Concert(ID_Concert),  
+    FOREIGN KEY ID_Groupe REFERENCES Groupe(ID_Groupe),
+    FOREIGN KEY ID_LineUp REFERENCES LineUp(ID_LineUp),  
+    FOREIGN KEY ID_Playlist REFERENCES Playlist(ID_Playlist),
+    FOREIGN KEY ID_Lieu REFERENCES Lieu(ID_Lieu)
+    CHECK (ID_Concert IS NOT NULL OR ID_Groupe IS NOT NULL OR 
+    ID_Playlist IS NOT NULL OR ID_LineUp IS NOT NULL 
+    OR ID_Lieu IS NOT NULL)
+    CHECK (Note IS NOT NULL OR Commentaire IS NOT NULL)
+    CHECK (Note BETWEEN 0 AND 5)
+);
+
+CREATE TABLE Amis(
+    ID_Utilisateur1 integer NOT NULL,
+    ID_Utilisateur2 integer NOT NULL,
+    UNIQUE(ID_Utilisateur1, ID_Utilisateur2),
+    FOREIGN KEY (ID_Utilisateur1) REFERENCES Utilisateur(ID_Utilisateur),  
+    FOREIGN KEY (ID_Utilisateur2) REFERENCES Utilisateur(ID_Utilisateur)
+);
+
+CREATE TABLE Follows(
+    ID_Suiveur integer NOT NULL,
+    ID_Suivi integer NOT NULL,
+    UNIQUE(ID_Suiveur, ID_Suivi),
+    FOREIGN KEY (ID_Suiveur) REFERENCES Utilisateur(ID_Utilisateur),  
+    FOREIGN KEY (ID_Suivi) REFERENCES Utilisateur(ID_Utilisateur)
 );
